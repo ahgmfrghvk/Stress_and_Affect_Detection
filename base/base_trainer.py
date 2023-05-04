f9d2ea55fc8429056a989d63ab0e81d262472ccf
ahgmfrghvk <80897270+ahgmfrghvk@users.noreply.github.com>
2023-05-04 17:00:37 +0900

    Update base_trainer.py

--- base/base_trainer.py
+++ base/base_trainer.py
@@ -202,16 +202,17 @@ class BaseTrainer:
         """
         setup GPU device if available, move model into configured device
         """
-        n_gpu = torch.cuda.device_count()
-        if n_gpu_use > 0 and n_gpu == 0:
-            self.logger.warning("Warning: There\'s no GPU available on this machine,"
+        #n_gpu = torch.cuda.device_count()
+        #if n_gpu_use > 0 and n_gpu == 0:
+        #    self.logger.warning("Warning: There\'s no GPU available on this machine,"
                                 "training will be performed on CPU.")
-            n_gpu_use = 0
-        if n_gpu_use > n_gpu:
-            self.logger.warning("Warning: The number of GPU\'s configured to use is {}, but only {} are available "
-                                "on this machine.".format(n_gpu_use, n_gpu))
-            n_gpu_use = n_gpu
-        device = torch.device('cuda:0' if n_gpu_use > 0 else 'cpu')
+        #    n_gpu_use = 0
+        #if n_gpu_use > n_gpu:
+        #    self.logger.warning("Warning: The number of GPU\'s configured to use is {}, but only {} are available "
+        #                        "on this machine.".format(n_gpu_use, n_gpu))
+        #    n_gpu_use = n_gpu
+        device = torch.device('cpu')
+        n_gpu_use = 0
         list_ids = list(range(n_gpu_use))
         return device, list_ids
 
